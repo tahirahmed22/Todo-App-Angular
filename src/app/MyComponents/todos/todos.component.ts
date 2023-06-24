d80ca60a5e01fe63b67bcf6e679611aa2d26a4fc
Tahir Ahmed <tahirahmed0333@protonmail.com>
2023-06-24 12:37:24 +0500

    1st commit; project have bugs; not working properly

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Todo } from "../../Todo";
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent {
+  todos!: Todo[];
+  localItem!: string;
+
+  constructor() {
+    this.localItem = localStorage.getItem("todos");
+    if (this.localItem == null) {
+      this.todos = [];
+    } else {
+      this.todos = JSON.stringify(this.localItem)
+    }
+  }
+
+  deleteTodo(todo: Todo) {
+    console.log(todo)
+    const index = this.todos!.indexOf(todo);
+    this.todos!.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+  addTodo(todo: Todo) {
+    console.log(todo)
+    this.todos!.push(todo);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+}
